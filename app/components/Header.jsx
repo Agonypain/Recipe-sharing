ede6d0c3b953bce425ad268c1418b0bf93160ca8
Diya <diyaothman.hussein@student.upt.ro>
2024-01-04 17:03:39 +0200

    created sign up page

---
+++ app/components/Header.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div>
+        <nav 
+        class="navbar bg-dark border-bottom border-body" 
+        data-bs-theme="dark"
+        >
+    <div class="container">
+        <Link href={"/"} class="navbar-brand">My Recipes</Link>
+        <Link href={"/signin"} class="btn btn-outline-success">Sign In</Link>
+    </div>
+  </nav>
+  </div>
+  )
+}
---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAA5FTbDQJ9a4YRcl7umsu0raIOqibvtYE",
+  authDomain: "recipesharing-84eec.firebaseapp.com",
+  projectId: "recipesharing-84eec",
+  storageBucket: "recipesharing-84eec.appspot.com",
+  messagingSenderId: "184200104530",
+  appId: "1:184200104530:web:fb94b69abcbe2bc03f6d8b"
+};
+
+const app = initializeApp(firebaseConfig);
